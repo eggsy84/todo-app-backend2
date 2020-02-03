bab521ccb71aa27dfa0e9cc31b840776a68da68c
James Heggs <james.heggs@techreturners.com>
2020-02-03 20:49:24 +0000

    Functioning JSON response

--- handler.js
+++ handler.js
@@ -5,9 +5,11 @@ const express = require('express');
 const app = express();
 
 app.get('/tasks', function (req, res) {
+  
   res.json({
     message: 'Your API works',
   });
 });
+
 
 module.exports.tasks = serverless(app);
