512e74d38cb8ad60cd2644b23d10e3248f7131b5
James Heggs <james.heggs@techreturners.com>
2020-02-11 20:25:42 +0000

    All endpoints mapped on backend

--- handler.js
+++ handler.js
@@ -3,11 +3,43 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const uuidv4 = require('uuid/v4');
 
 app.get('/tasks', function (req, res) {
+
+  const someTasks = [
+    {
+      taskId: uuidv4(),
+      description: "Buy some milk",
+      completed: false
+    },
+    {
+      taskId: uuidv4(),
+      description: "Test API in postman",
+      completed: false
+    }
+  ]
   
   res.json({
-    message: 'Your API works',
+    tasks: someTasks
+  });
+});
+
+app.post('/tasks', function (req, res) {
+  res.json({
+    message: 'Your POST works',
+  });
+});
+
+app.put('/tasks/:taskId', function (req, res) {
+  res.json({
+    message: 'Your PUT works',
+  });
+});
+
+app.delete('/tasks/:taskId', function (req, res) {
+  res.json({
+    message: 'Your DELETE works',
   });
 });
 
