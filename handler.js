ebd48370576c29c840ede46154d7b1fbbea72346
James Heggs <james.heggs@techreturners.com>
2020-02-19 16:26:45 +0000

    Successful creation of tasks

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+app.use(express.json());
 const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
@@ -34,8 +35,25 @@ app.get('/tasks', function (req, res) {
 
 // Creating tasks
 app.post('/tasks', function (req, res) {
-  res.json({
-    message: 'Your POST works',
+
+  // Accept information from the client
+  // about what task is being created
+  const taskToInsert = req.body;
+  taskToInsert.taskId = uuidv4();
+
+  // Take that information and pre-populate an SQL INSERT statement
+  // Execute the statement
+  connection.query('INSERT INTO `task` SET ?', taskToInsert, function (error, results, fields) {
+    if(error) {
+      console.error("Your query had a problem with insert a new task", error);
+      res.status(500).json({errorMessage: error});
+    }
+    else {
+      // Return to the client information about the task that has been created
+      res.json({
+        task: taskToInsert
+      });
+    }
   });
 });
 
